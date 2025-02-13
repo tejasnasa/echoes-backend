f95569a7aab4456ccc5ae0d42b3b84b092e3034f
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2025-02-13 23:46:59 +0530

    Fix bugs and add comments

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -11,22 +11,24 @@ export const signup = async ({
   password,
 }: typeof user.$inferInsert) => {
   try {
+    // Check for existing username
     const existingUsername = db
       .select()
       .from(user)
       .where(eq(user.username, username));
-
     if ((await existingUsername).length > 0) {
-      return new ServerResponse(false, "Username already exists", null, 409);
+      throw new Error("Username already exists");
     }
 
+    // Check for existing username
     const existingEmail = db.select().from(user).where(eq(user.email, email));
     if ((await existingEmail).length > 0) {
-      return new ServerResponse(false, "Email already exists", null, 409);
+      throw new Error("Email already exists");
     }
 
-    const hashedPassword = await hash(password, 10);
+    const hashedPassword = await hash(password, 10); // Password hash
 
+    // Inserting values
     const newUser = db
       .insert(user)
       .values({
@@ -35,12 +37,19 @@ export const signup = async ({
         email,
         password: hashedPassword,
       })
-      .returning();
-    console.log(newUser);
+      .returning({ id: user.id });
 
-    return new ServerResponse(true, "User created", await newUser, 201);
+    return new ServerResponse(true, "User created", await newUser, 201); // Returning successful response
   } catch (error) {
     console.log(error);
-    return new ServerResponse(false, "Server error", error, 400);
+
+    if (
+      error.message === "Email already exists" ||
+      error.message === "Username already exists"
+    ) {
+      return new ServerResponse(false, error.message, null, 409); // Returning unsuccessful response
+    }
+
+    return new ServerResponse(false, "Server error", error, 400); // Returning unsuccessful response
   }
 };
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -4,11 +4,10 @@ import { signup } from "../controllers/auth";
 const authRouter = express.Router();
 
 authRouter.post("/signup", async (req: Request, res: Response) => {
-  console.log(req.body);
   const { username, fullname, email, password } = req.body;
 
   const response = await signup({ username, fullname, email, password });
-  console.log(response);
+  
   res.status(response.statusCode).json(response);
 });
 
