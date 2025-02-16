6254a8956f232308a1cad7be349721dc4049fb03
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2025-02-16 23:45:54 +0530

    Add error handling and comments to user controllers

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -4,29 +4,41 @@ import { db } from "../index";
 import { ServerResponse } from "../models/serverResponse";
 
 export const getUserData = async (userSerId: string) => {
-  const userData = await db
-    .select({
-      serialId: user.serialId,
-      username: user.username,
-      fullname: user.username,
-      profile_pic: user.profile_pic,
-      bio: user.bio,
-      cover_pic: user.cover_pic,
-      post: {
-        serialId: post.serialId,
-        text: post.text,
-        images: post.images,
-      },
-    })
-    .from(user)
-    .where(eq(user.serialId, Number(userSerId)))
-    .leftJoin(post, eq(post.userId, user.id));
+  try {
+    // Fetch user data
+    const userData = await db
+      .select({
+        serialId: user.serialId,
+        username: user.username,
+        fullname: user.username,
+        profile_pic: user.profile_pic,
+        bio: user.bio,
+        cover_pic: user.cover_pic,
+        post: {
+          serialId: post.serialId,
+          text: post.text,
+          images: post.images,
+        },
+      })
+      .from(user)
+      .where(eq(user.serialId, Number(userSerId)))
+      .leftJoin(post, eq(post.userId, user.id));
 
-  if (!userData.length) {
-    throw new Error("User doesn't exist");
+    // Error case for user not existing
+    if (!userData.length) {
+      throw new Error("User doesn't exist");
+    }
+
+    return new ServerResponse(true, "User data fetched", userData[0], 200); // Successful response
+  } catch (error) {
+    console.log(error);
+
+    if (error.message === "User doesn't exist") {
+      return new ServerResponse(false, error.message, error, 404); // Unsuccessful response
+    }
+
+    return new ServerResponse(false, "Internal server error", error, 400); // Unsuccessful response
   }
-
-  return new ServerResponse(true, "User data fetched", userData[0], 200);
 };
 
 export const followUser = async ({
@@ -36,68 +48,82 @@ export const followUser = async ({
   followerId: string;
   toBeFollowedSerId: string;
 }) => {
-  const [toBeFollowedUser] = await db
-    .select({
-      id: user.id,
-    })
-    .from(user)
-    .where(eq(user.serialId, Number(toBeFollowedSerId)))
-    .limit(1);
+  try {
+    // Fetch id of the user to be followed
+    const [toBeFollowedUser] = await db
+      .select({
+        id: user.id,
+      })
+      .from(user)
+      .where(eq(user.serialId, Number(toBeFollowedSerId)))
+      .limit(1);
 
-  const existingFollow = await db
-    .select()
-    .from(follow)
-    .where(
-      and(
-        eq(follow.followerId, followerId),
-        eq(follow.followingId, toBeFollowedUser.id)
-      )
-    )
-    .limit(1);
-
-  if (existingFollow.length) {
-    // Unfollow
-    await db
-      .delete(follow)
+    // Check if the follow already exists
+    const existingFollow = await db
+      .select()
+      .from(follow)
       .where(
         and(
           eq(follow.followerId, followerId),
           eq(follow.followingId, toBeFollowedUser.id)
         )
-      );
+      )
+      .limit(1);
 
-    return new ServerResponse(true, "Unfollowed user", null, 200);
+    if (existingFollow.length) {
+      // If follow exists, unfollow user
+      await db
+        .delete(follow)
+        .where(
+          and(
+            eq(follow.followerId, followerId),
+            eq(follow.followingId, toBeFollowedUser.id)
+          )
+        );
+
+      return new ServerResponse(true, "Unfollowed user", null, 200); // Successful response
+    }
+
+    // If follow doesn't exist, follow user
+    await db.insert(follow).values({
+      followerId,
+      followingId: toBeFollowedUser.id,
+    });
+
+    return new ServerResponse(true, "Followed user", null, 200); // Successful response
+  } catch (error) {
+    console.log(error);
+
+    return new ServerResponse(false, "Internal server error", error, 400); // Unsuccessful response
   }
-
-  // Follow
-  await db.insert(follow).values({
-    followerId,
-    followingId: toBeFollowedUser.id,
-  });
-
-  return new ServerResponse(true, "Followed user", null, 200);
 };
 
 export const searchUsers = async (query: string) => {
-  const searchQuery = query.trim();
-
-  const users = await db
-    .select({
-      serialId: user.serialId,
-      username: user.username,
-      fullname: user.fullname,
-      profile_pic: user.profile_pic,
-    })
-    .from(user)
-    .where(
-      or(
-        ilike(user.username, `%${searchQuery}%`),
-        ilike(user.fullname, `%${searchQuery}%`)
+  try {
+    const searchQuery = query.trim();
+    // Perform search by username and fullname
+    const users = await db
+      .select({
+        serialId: user.serialId,
+        username: user.username,
+        fullname: user.fullname,
+        profile_pic: user.profile_pic,
+      })
+      .from(user)
+      .where(
+        or(
+          ilike(user.username, `%${searchQuery}%`),
+          ilike(user.fullname, `%${searchQuery}%`)
+        )
       )
-    )
-    .limit(5);
-  
-    return new ServerResponse(true, "Searched users fetched", users, 200)
+      .limit(5);
+
+    return new ServerResponse(true, "Searched users fetched", users, 200); // Succcessful response
+  } catch (error) {
+    console.log(error);
+
+    return new ServerResponse(false, "Internal server error", error, 400); // Unsuccessful response
+  }
 };
 
 export const getRecommendedUsers = () => {};
