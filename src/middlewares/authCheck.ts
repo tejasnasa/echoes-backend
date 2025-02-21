b1a8cab900c2208de80110d606e68fe864bccb1a
tejasnasa <tejasnasa2005@gmail.com>
2025-02-21 14:24:59 +0530

    Create token expiry error

--- src/middlewares/authCheck.ts
+++ src/middlewares/authCheck.ts
@@ -7,17 +7,31 @@ const authCheck = (req: Request, res: Response, next: NextFunction) => {
   if (!token) {
     const response = new ServerResponse(true, "No token provided", null, 401);
     res.status(response.statusCode).json(response);
+    return;
   }
 
   try {
     const decoded = jwt.verify(token, `${process.env.JWT_SECRET}`) as {
       userId: string;
+      exp: number;
     };
+
+    if (Math.floor(Date.now() / 1000) > decoded.exp) {
+      throw new Error("Token expired, login again");
+    }
+
     req.body.token = decoded;
     console.log(decoded);
+
     next();
   } catch (error) {
-    const response = new ServerResponse(true, "Invalid token", error, 401);
+    if (error.message === "Token expired, login again") {
+      const response = new ServerResponse(false, error.message, null, 403);
+
+      res.status(response.statusCode).json(response);
+    }
+
+    const response = new ServerResponse(false, "Invalid token", error, 401);
     res.status(response.statusCode).json(response);
   }
 };
